9ceabe3ed220933ba9064b1b35df3378f2936dab
Stefan Turner <stefanturner@MacBook-Air-von-Stefan.local>
2022-12-30 17:12:32 +0000

    initial commit

---
+++ webpack.common.js
@@ -0,0 +1,39 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const WebpackPrettierPlugin = require("webpack-prettier-plugin");
+
+module.exports = {
+  entry: "./src/js/app.js",
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/html/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new WebpackPrettierPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: {
+          loader: "html-loader",
+          options: {
+            preprocessor: (content, loaderContext) =>
+              content.replace(
+                /<include src="(.+)"\s*\/?>(?:<\/include>)?/gi,
+                (m, src) => {
+                  const filePath = path.resolve(loaderContext.context, src);
+                  loaderContext.dependency(filePath);
+                  return fs.readFileSync(filePath, "utf8");
+                }
+              ),
+          },
+        },
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
